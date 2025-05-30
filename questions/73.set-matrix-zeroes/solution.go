1193f3e934cc75a00414caa3937f048de77a256f
aryanpnd <captainaryan9870@gmail.com>
2025-05-31 02:40:11 +0530

    better approach with enhanced time and space complexity in Set Matrix Zeroes solution

--- questions/73.set-matrix-zeroes/solution.go
+++ questions/73.set-matrix-zeroes/solution.go
@@ -7,6 +7,8 @@
 // @lc code=start
 
 // brute force, setting the zeros to -97 and then resetting them back to zero
+// time complexity: O((m × n) × (m + n))
+// space complexity: O(1) - no extra space used
 // func setZeroes(matrix [][]int) {
 // 	rows := len(matrix)
 // 	if rows == 0 {
@@ -46,6 +48,37 @@
 // 	for i := 0; i < len(matrix); i++ {
 // 		for j := 0; j < len(matrix[0]); j++ {
 // 			if matrix[i][j] == -97 {
+// 				matrix[i][j] = 0
+// 			}
+// 		}
+// 	}
+// }
+
+// better approach - using two maps to store the count of zeros
+// time complexity: O(2 × m × n)
+// space complexity: O(n) + O(m) - no extra space used
+// func setZeroes(matrix [][]int) {
+// 	rowZerosCount := make(map[int]bool)
+// 	columnZerosCount := make(map[int]bool)
+
+// 	for i := 0; i < len(matrix); i++ {
+// 		for j := 0; j < len(matrix[i]); j++ {
+// 			if matrix[i][j] == 0 {
+// 				rowZerosCount[i] = true
+// 				columnZerosCount[j] = true
+// 			}
+// 		}
+// 	}
+
+// 	for i := range rowZerosCount {
+// 		for j := 0; j < len(matrix[i]); j++ {
+// 			matrix[i][j] = 0
+// 		}
+// 	}
+
+// 	for j := range columnZerosCount {
+// 		for i := 0; i < len(matrix); i++ {
+// 			if j < len(matrix[i]) {
 // 				matrix[i][j] = 0
 // 			}
 // 		}
