737468f271e0a84f67f0ac7231c74ba8e1724d3f
aryanpnd <captainaryan9870@gmail.com>
2025-06-01 00:32:17 +0530

    refactor: Optimize maximumTripletValue function for clarity and performance

--- questions/2873.maximum-value-of-an-ordered-triplet-i/solution.go
+++ questions/2873.maximum-value-of-an-ordered-triplet-i/solution.go
@@ -6,7 +6,27 @@
 
 // @lc code=start
 func maximumTripletValue(nums []int) int64 {
-    
+	numsLen := len(nums)
+
+	if numsLen <= 0 {
+		return 0
+	}
+
+	maxVal := 0
+
+	for i := 0; i < numsLen-2; i++ {
+		for j := i + 1; j < numsLen-1; j++ {
+			for k := j + 1; k < numsLen; k++ {
+				currentVal := (nums[i] - nums[j]) * nums[k]
+				if currentVal > maxVal {
+					maxVal = currentVal
+				}
+			}
+		}
+	}
+
+	return int64(maxVal)
 }
+
 // @lc code=end
 
